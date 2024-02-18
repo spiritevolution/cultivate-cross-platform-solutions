b55b582e4886bc6bcf794f2560711f9d95cac635
spiritevolution <158512066+spiritevolution@users.noreply.github.com>
2024-02-18 15:42:22 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,41 @@
+        }
+    }
+public class RandomDataGenerator {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);            System.out.println("Random Number: " + item);
+        }
+
+
+        int[] data = new int[10];
+    public static void main(String[] args) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+        }
+}
+    });
+
+    const data = generateRandomData();
+    return data;
+}
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+    data.forEach(item => {
+main();
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+function main() {
+        console.log("Random Number: " + item);
